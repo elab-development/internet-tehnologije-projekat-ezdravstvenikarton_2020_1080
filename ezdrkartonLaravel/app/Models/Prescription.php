f9c93f9dfada990d79624882fcb9c6557f46998e
StankovS <ss20201080@student.fon.bg.ac.rs>
2024-01-04 19:39:25 +0100

    dodata klasa Presrciption

---
+++ ezdrkartonLaravel/app/Models/Prescription.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prescription extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'patient_id', 'doctor_id', 'medication', 'dosage', 'instructions', 'issue_date',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+}

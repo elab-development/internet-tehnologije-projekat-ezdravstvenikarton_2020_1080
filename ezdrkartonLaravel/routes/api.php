3570cccab99941863ffbd17a5ec0c2c9b14545f0
urosilic22 <urosilic.20@gmail.com>
2024-01-06 00:11:13 +0100

    dodatno zasticene role tako da korisnici odredjene uloge imaju odredjene funkcionalnosti

--- ezdrkartonLaravel/routes/api.php
+++ ezdrkartonLaravel/routes/api.php
@@ -36,17 +36,32 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::post('/logout',[AuthController::class,'logout']);
 
-    Route::get('/records/pretraga',[MedicalRecordController::class,'pretraga']);
-    Route::get('/records',[MedicalRecordController::class,'index']);
-    Route::get('/records/{id}',[MedicalRecordController::class,'show']);
-    Route::post('/records',[MedicalRecordController::class,'store']);
+
+    Route::middleware('role:doctor')->group(function () {
+        Route::get('/records',[MedicalRecordController::class,'index']);
+        Route::get('/records/{id}',[MedicalRecordController::class,'show']);
+        Route::post('/records',[MedicalRecordController::class,'store']);
+        
+        Route::put('/records/{id}',[MedicalRecordController::class,'update']);
+        Route::delete('/records/{id}',[MedicalRecordController::class,'destroy']);
+
+
+        Route::resource('/prescriptions',PrescriptionController::class);
+    });
+
+
+    Route::middleware('role:patient')->group(function () {
+        Route::get('/records/pretraga',[MedicalRecordController::class,'pretraga']);
+   
+        Route::get('/appointments/pretraga',[AppointmentController::class,'pretraga']);
+        Route::resource('/appointments',AppointmentController::class);
+        Route::get('/prescriptions/pretraga',[PrescriptionController::class,'pretraga']);
+
+
+    });
     
-    Route::put('/records/{id}',[MedicalRecordController::class,'update']);
-    Route::delete('/records/{id}',[MedicalRecordController::class,'destroy']);
-    Route::get('/appointments/pretraga',[AppointmentController::class,'pretraga']);
-    Route::resource('/appointments',AppointmentController::class);
-    Route::get('/prescriptions/pretraga',[PrescriptionController::class,'pretraga']);
-    Route::resource('/prescriptions',PrescriptionController::class);
+   
+   
 });
  
 

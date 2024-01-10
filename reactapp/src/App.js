4fe6f58f49197e0e008f90341bb7420e1939afb9
urosilic22 <urosilic.20@gmail.com>
2024-01-10 22:00:45 +0100

    dodate rute

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './komponente/pocetna/Home';
 import ImageGallery from './komponente/Galerija/ImageGallery';
 
 function App() {
   return (
-    <div className="App">
-       <Home></Home>
-       <ImageGallery></ImageGallery>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<ImageGallery />} />
+         
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

72e7c8a9019847598777a2e9da93f96522f266b6
StankovS <ss20201080@student.fon.bg.ac.rs>
2024-01-11 20:40:40 +0100

    dodata pretraga doktora po imenu i prezimenu

--- reactapp/src/komponente/Doktori/Doctors.jsx
+++ reactapp/src/komponente/Doktori/Doctors.jsx
@@ -3,27 +3,39 @@ import './Doctors.css';
 import useDoctors from '../customHooks/useDoctors';
 import DoctorRow from './DoctorRow';
 
- 
-
-function Doctors({doctors,deleteDoctor}) {
+function Doctors({ doctors, deleteDoctor }) {
   const [filter, setFilter] = useState('All');
+  const [searchQuery, setSearchQuery] = useState('');
 
   const specialties = ["All", "Cardiology", "Neurology", "Pediatrics", "Orthopedics", "Dermatology"];
 
-  const filteredDoctors = filter === 'All' ? doctors : doctors.filter(doctor => doctor.specialty === filter);
+  const filteredDoctors = doctors.filter(doctor => {
+    return (filter === 'All' || doctor.specialty === filter) &&
+           (doctor.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  });
+
   return (
     <div className="doctors-container">
-       <div className="filter-container">
-            {specialties.map(specialty => (
-              <button
-                key={specialty}
-                className={`filter-button ${filter === specialty ? 'active' : ''}`}
-                onClick={() => setFilter(specialty)}
-              >
-                {specialty}
-              </button>
-            ))}
-          </div>
+      <div className="search-and-filter-container">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="filter-container">
+          {specialties.map(specialty => (
+            <button
+              key={specialty}
+              className={`filter-button ${filter === specialty ? 'active' : ''}`}
+              onClick={() => setFilter(specialty)}
+            >
+              {specialty}
+            </button>
+          ))}
+        </div>
+      </div>
       <table>
         <thead>
           <tr>
@@ -36,9 +48,9 @@ function Doctors({doctors,deleteDoctor}) {
           </tr>
         </thead>
         <tbody>
-            {filteredDoctors.map(doctor => (
-                <DoctorRow key={doctor.id} doctor={doctor} deleteDoctor={deleteDoctor} />
-            ))}
+          {filteredDoctors.map(doctor => (
+            <DoctorRow key={doctor.id} doctor={doctor} deleteDoctor={deleteDoctor} />
+          ))}
         </tbody>
       </table>
     </div>

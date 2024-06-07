f8bb391152c3f3c6bac83f0f139c761097f25933
StankovS <ss20201080@student.fon.bg.ac.rs>
2024-06-07 23:22:38 +0200

    kreirana login komponenta

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './komponente/pocetna/Home';
@@ -7,8 +7,10 @@ import Doctors from './komponente/Doktori/Doctors';
 import DoctorForm from './komponente/Doktori/DoctorForm';
 import Navbar from './komponente/navbar/Navbar';
 import useDoctors from './komponente/customHooks/useDoctors';
+import Login from './komponente/Login/Login';
 
 function App() {
+  const [user,setUser] = useState(null);
   const { doctors,addDoctor, deleteDoctor } = useDoctors();
   return (
     <Router>
@@ -20,6 +22,16 @@ function App() {
           <Route path="/doctors/add" element={<DoctorForm addDoctor={addDoctor} />} />
           <Route path="/doctors" element={<Doctors  doctors={doctors}  deleteDoctor={deleteDoctor}/>} />
           <Route path="/gallery" element={<ImageGallery />} />
+
+
+
+
+          {/*seminarski */}
+          <Route path="/login" element={<Login />} />
+
+
+
+
         </Routes>
       </div>
     </Router>
---
+++ reactapp/src/komponente/Login/Login.js
@@ -0,0 +1,56 @@
+ 
+import React, { useState } from 'react';
+import axios from 'axios';
+ 
+
+const Login = ({ setUser }) => {
+  const [email, setEmail] = useState('cordelia.kulas@example.org');
+  const [password, setPassword] = useState('password123');
+  const [error, setError] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/login', { email, password });
+      const { access_token, user } = response.data;
+      sessionStorage.setItem('token', access_token);
+      setUser(user);
+    } catch (error) {
+      setError('Invalid credentials. Please try again.');
+    }
+  };
+
+  return (
+    <div className="doctor-form-container">
+      <form onSubmit={handleLogin} className="doctor-form">
+        <h2>Login</h2>
+        {error && <p className="error">{error}</p>}
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="submit-btn">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
